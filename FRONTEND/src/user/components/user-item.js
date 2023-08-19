cc872e34efc16240e5b9a3195f2dbe602e72dc22
Marcel Filejecki <filej1025@gmail.com>
2023-08-19 18:25:30 +0200

    add wrapping card component

--- FRONTEND/src/user/components/user-item.js
+++ FRONTEND/src/user/components/user-item.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 
 import Avatar from "../../shared/components/UI/Avatar";
+import Card from "../../shared/components/UI/Card";
 import styles from "./user-item.module.css";
 
 const UserItem = (props) => {
@@ -9,17 +11,19 @@ const UserItem = (props) => {
 
   return (
     <li key={id} className={styles["user-item"]}>
-      <div className={styles["user-item__content"]}>
-        <div className={styles["user-item__image"]}>
-          <Avatar image={image} alt={name} />
-        </div>
-        <div className={styles["user-item__info"]}>
-          <h2>{name}</h2>
-          <h3>
-            {placeCount} {places}
-          </h3>
-        </div>
-      </div>
+      <Card className={styles["user-item__content"]}>
+        <Link to={`/${id}/places`}>
+          <div className={styles["user-item__image"]}>
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className={styles["user-item__info"]}>
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {places}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
