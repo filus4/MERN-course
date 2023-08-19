74c057db93681f1a4829d1e45805eb809ff505f4
Marcel Filejecki <filej1025@gmail.com>
2023-08-19 21:04:21 +0200

    add custom button

--- FRONTEND/src/places/components/place-item.js
+++ FRONTEND/src/places/components/place-item.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../../shared/components/UI/Card";
+import Button from "../../shared/components/FormElements/Button";
 import styles from "./place-item.module.css";
 
 const PlaceItem = (props) => {
@@ -18,9 +19,9 @@ const PlaceItem = (props) => {
           <p>{description}</p>
         </div>
         <div className={styles["place-item__actions"]}>
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
