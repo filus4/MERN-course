707cab8cc4131187ec9f80b0d2659de9c4a5331d
Marcel Filejecki <filej1025@gmail.com>
2023-08-23 23:13:59 +0200

    small changes

--- FRONTEND/src/shared/components/Navigation/nav-links.js
+++ FRONTEND/src/shared/components/Navigation/nav-links.js
@@ -10,35 +10,30 @@ const NavLinks = (props) => {
   const auth = useContext(AuthContext);
 
   return (
-    <ul className={styles["nav-links"]}>
-      <li onClick={toggleDrawer}>
+    <ul className={styles["nav-links"]} onClick={toggleDrawer}>
+      <li>
         <NavLink to="/" exact>
           ALL USERS
         </NavLink>
       </li>
       {auth.isLoggedIn && (
-        <li onClick={toggleDrawer}>
+        <li>
           <NavLink to="/u1/places">MY PLACES</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
-        <li onClick={toggleDrawer}>
+        <li>
           <NavLink to="/places/new">ADD PLACE</NavLink>
         </li>
       )}
       {!auth.isLoggedIn && (
-        <li onClick={toggleDrawer}>
+        <li>
           <NavLink to="/auth">AUTHENTICATE</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
-        <li
-          onClick={() => {
-            auth.logout();
-            toggleDrawer();
-          }}
-        >
-          <NavLink to="/">Logout</NavLink>
+        <li onClick={auth.logout}>
+          <NavLink to="/">LOGOUT</NavLink>
         </li>
       )}
     </ul>
