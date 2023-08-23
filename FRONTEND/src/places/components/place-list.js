4b230c945871f9c5f2dc55591b5d9dbff0577732
Marcel Filejecki <filej1025@gmail.com>
2023-08-23 19:00:54 +0200

    small styling change

--- FRONTEND/src/places/components/place-list.js
+++ FRONTEND/src/places/components/place-list.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Card from "../../shared/components/UI/Card";
 import PlaceItem from "./place-item";
+import Button from "../../shared/components/FormElements/Button";
 import styles from "./place-list.module.css";
 
 const PlaceList = (props) => {
@@ -11,7 +12,7 @@ const PlaceList = (props) => {
       <div className={`${styles["place-list"]} center`}>
         <Card>
           <h2>No places found. Maybe create one?</h2>
-          <button>Share Place</button>
+          <Button to="/places/new">Share Place</Button>
         </Card>
       </div>
     );
