0d4b39823ef7ee1b28c84821d1adf8e74b407e98
Marcel Filejecki <filej1025@gmail.com>
2023-08-27 01:00:14 +0200

    add general route not found error

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,12 +3,17 @@ const bodyParser = require("body-parser");
 
 const placesRouter = require("./routes/places-routes");
 const usersRouter = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRouter);
+
+app.use((req, res, next) => {
+  return next(new HttpError("Could not find this route.", 404));
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
