2715b87a3711e4177e78709403128160789bb9d1
Marcel Filejecki <filej1025@gmail.com>
2023-09-01 22:39:04 +0200

    getting all users from backend works

--- FRONTEND/src/user/pages/users.js
+++ FRONTEND/src/user/pages/users.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UsersList from "../components/users-list";
-
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Marcel",
-    image:
-      "https://static.planetminecraft.com/files/image/minecraft/project/2021/199/14520222-aincradrender_xl.webp",
-    places: "3",
-  },
-];
+import ErrorModal from "../../shared/components/UI/ErrorModal";
+import LoadingSpinner from "../../shared/components/UI/LoadingSpinner";
 
 const Users = () => {
+  const [fetchedUsers, setFetchedUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+
+        setFetchedUsers(data.users);
+      } catch (err) {
+        setError(err);
+      }
+      setIsLoading(false);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <>
-      <UsersList items={DUMMY_USERS} />
+      <ErrorModal error={error} onClear={() => setError(false)} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && fetchedUsers && <UsersList items={fetchedUsers} />}
     </>
   );
 };
