d6deb87c52eb5346e0cd06f7cefbf5a31c18c8ce
Marcel Filejecki <filej1025@gmail.com>
2023-09-01 22:18:22 +0200

    api call to login now works

--- FRONTEND/src/user/pages/auth.js
+++ FRONTEND/src/user/pages/auth.js
@@ -35,11 +35,34 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
 
     if (isLoginForm) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -56,11 +79,10 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(data.message);
         }
-        console.log(data);
+
         setIsLoading(false);
         auth.login();
       } catch (err) {
-        console.log(err);
         setIsLoading(false);
         setError(err.message || "Something went wrong, please try again.");
       }
