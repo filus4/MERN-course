f72c8582c3a0df432e3f37eba7b15b1f02693162
Marcel Filejecki <filej1025@gmail.com>
2023-09-01 18:28:23 +0200

    getting all user's places with populate method

--- BACKEND/controllers/places-controller.js
+++ BACKEND/controllers/places-controller.js
@@ -30,23 +30,31 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  // let places;
+
+  // other approach
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    // places = await Place.find({ creator: userId });
+
+    // other approach
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     return next(
       new HttpError("Fetching places failed, please try again.", 500)
     );
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError("Could not find any places for the provided user id.", 404)
     );
   }
 
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
