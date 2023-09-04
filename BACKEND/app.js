0b92b027145334e2e3056512d4c8eb7d3cb3df82
Marcel Filejecki <filej1025@gmail.com>
2023-09-04 19:56:35 +0200

    serving images statically

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -11,6 +12,8 @@ const db = require("./util/api");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
