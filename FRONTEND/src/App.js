57830bee19de6ec41a2c414cb29c55bc4b3b0e17
Marcel Filejecki <filej1025@gmail.com>
2023-09-05 20:39:32 +0200

    add token expiration date

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -18,10 +18,19 @@ function App() {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(null);
 
-  const login = useCallback((userId, token) => {
+  const login = useCallback((userId, token, expirationDate) => {
     setToken(token);
-    localStorage.setItem("userData", JSON.stringify({ userId, token }));
     setUserId(userId);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId,
+        token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
   const logout = useCallback(() => {
     setToken(null);
@@ -31,8 +40,16 @@ function App() {
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
