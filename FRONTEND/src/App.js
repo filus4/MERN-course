5659981bb3fc4cc5fd56270c553f790180d0167f
Marcel Filejecki <filej1025@gmail.com>
2023-09-05 20:33:45 +0200

    add auto login with usage of local storage

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -20,15 +20,23 @@ function App() {
 
   const login = useCallback((userId, token) => {
     setToken(token);
+    localStorage.setItem("userData", JSON.stringify({ userId, token }));
     setUserId(userId);
   }, []);
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
 
-  let routes;
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (storedData && storedData.token) {
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
 
+  let routes;
   if (token) {
     routes = (
       <Switch>
